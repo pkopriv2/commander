b511973d2ade88c7266a96595f892586a1572b1c
Preston Koprivica <pkopriv2@gmail.com>
2013-11-17 23:13:54 -0600

    Preparing for development iteration

--- project.sh
+++ project.sh
@@ -1,5 +1,5 @@
 name          "commander"
-version       "1.0.0"
+version       "1.0.1-SNAPSHOT"
 description   "Commander is a tool to facilitate creating multi command tools" 
 
 file          "commands"
